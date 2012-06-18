b050ea1ad8705f85f5fb08158db494c4bbbc0544
Liam McLennan <liam.mclennan@gmail.com>
2012-06-18 20:52:54 +1000

    added a test

---
+++ test/config_tests.js
@@ -0,0 +1,49 @@
+describe('config', function () {
+    var config = require('../lib/config'),
+        should = require('should');
+
+    it('should exist', function () {
+        should.exist(config);                
+    });
+
+    describe('validation', function () {
+
+        describe('correct pathToRepo', function () {
+            before(function () {
+                config.data = {
+                    pathToRepo: './' // is a git repo
+                };
+            });
+        
+            it('should validate', function () {
+                config.validate();                    
+            });    
+        
+        });
+
+        describe('wrong pathToRepo', function () {
+            before(function () {
+                config.data = {
+                    pathToRepo: './test' // not a git repo
+                };
+            });
+        
+            it('should fail validate', function () {
+                (function () {
+                    config.validate();   
+                }).should.throw();
+            });    
+        
+        });
+
+
+        
+        it('should error if the ', function () {
+                        
+                        
+                    
+        });         
+    
+    });
+    
+});
